91cba2d4d86b5809fc4c83e017b123258e2f0da3
siddheswarojha <siddheswarojha23@gmail.com>
2022-11-16 18:29:21 +0530

    initializing empty git repo

---
+++ src/test/java/com/xprience/xprience/XprienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xprience.xprience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XprienceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
